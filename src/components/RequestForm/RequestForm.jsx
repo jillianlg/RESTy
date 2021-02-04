50c116aef9a304cfba3ad9a3833731abdb13ad1f
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-04 14:10:16 -0800

    adds POST input radio button with method and onChange and lable, AND cleans up URL and GET formating

--- src/components/RequestForm/RequestForm.jsx
+++ src/components/RequestForm/RequestForm.jsx
@@ -5,22 +5,23 @@ const RequestForm = ({ url, method, onSubmit, onChange }) => {
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input 
-          type="text" 
-          name="url" 
-          placeholder="URL" 
-          value={url} 
+        <input type="text" name="url" placeholder="URL" value={url} 
           onChange={onChange} />
       </form>
       <div>
-        <input 
-          type="radio" 
-          id="get" 
-          name="method" 
-          value="GET" 
+        {/* GET method */}
+        <input type="radio" id="get" name="method" value="GET" 
           checked={method === 'GET'} 
           onChange={onChange} />
         <label htmlFor="get">GET</label>
+        {/* POST method */}
+        <input type="radio" id="post" name="method" value="POST" 
+          checked={method === 'POST'} 
+          onChange={onChange} />
+        <label htmlFor="post">POST</label>
+        {/* PUT method */}
+        {/* PATCH method */}
+        {/* DELETE method */}
       </div>
     </div>
 
