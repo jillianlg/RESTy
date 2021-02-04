ff3aef742ddf461e7c5adea3068bffaca3cf3680
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-04 15:16:46 -0800

    adds css to RequestForm

--- src/components/RequestForm/RequestForm.jsx
+++ src/components/RequestForm/RequestForm.jsx
@@ -1,41 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './RequestForm.css';
 
 const RequestForm = ({ url, method, body, onSubmit, onChange }) => {
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input type="text" name="url" placeholder="URL" value={url} 
+      <form className={styles.RequestForm} onSubmit={onSubmit}>
+        <input className={styles.url} 
+          type="text" name="url" placeholder="URL" value={url} 
           onChange={onChange} />
-      
-        <div>
-          {/* GET method */}
-          <input type="radio" id="get" name="method" value="GET" 
-            checked={method === 'GET'} 
-            onChange={onChange} />
-          <label htmlFor="get">GET</label>
-          {/* POST method */}
-          <input type="radio" id="post" name="method" value="POST" 
-            checked={method === 'POST'} 
-            onChange={onChange} />
-          <label htmlFor="post">POST</label>
-          {/* PUT method */}
-          <input type="radio" id="put" name="method" value="PUT"
-            checked={method === 'PUT'}
-            onChange={onChange} />
-          <label htmlFor="put">PUT</label>
-          {/* PATCH method */}
-          <input type="radio" id="patch" name="method" value="PATCH"
-            checked={method === 'PATCH'}
-            onChange={onChange} /> 
-          <label htmlFor="patch">PATCH</label>
-          {/* DELETE method */}
-          <input type="radio" id="delete" name="method" value="DELETE"
-            checked={method === 'DELETE'}
-            onChange={onChange} />
-          <label htmlFor="delete">DELETE</label>
-        </div>
-
+        <section>
+          <div>
+            {/* GET method */}
+            <input type="radio" id="get" name="method" value="GET" 
+              checked={method === 'GET'} 
+              onChange={onChange} />
+            <label htmlFor="get">GET</label>
+            {/* POST method */}
+            <input type="radio" id="post" name="method" value="POST" 
+              checked={method === 'POST'} 
+              onChange={onChange} />
+            <label htmlFor="post">POST</label>
+            {/* PUT method */}
+            <input type="radio" id="put" name="method" value="PUT"
+              checked={method === 'PUT'}
+              onChange={onChange} />
+            <label htmlFor="put">PUT</label>
+            {/* PATCH method */}
+            <input type="radio" id="patch" name="method" value="PATCH"
+              checked={method === 'PATCH'}
+              onChange={onChange} /> 
+            <label htmlFor="patch">PATCH</label>
+            {/* DELETE method */}
+            <input type="radio" id="delete" name="method" value="DELETE"
+              checked={method === 'DELETE'}
+              onChange={onChange} />
+            <label htmlFor="delete">DELETE</label>
+          </div>
+        </section>
+        
         <button>Submit</button>
 
         <textarea placeholder="Raw JSON Body" name="body" value={body}
