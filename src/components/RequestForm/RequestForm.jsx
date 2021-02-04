226a1d78623c4e4c07f0e64a398381bfcd72cf19
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-04 14:09:13 -0800

    adds GET input radio button with method and onChange and lable

--- src/components/RequestForm/RequestForm.jsx
+++ src/components/RequestForm/RequestForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const RequestForm = ({ url, onSubmit, onChange }) => {
+const RequestForm = ({ url, method, onSubmit, onChange }) => {
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -12,13 +12,25 @@ const RequestForm = ({ url, onSubmit, onChange }) => {
           value={url} 
           onChange={onChange} />
       </form>
+      <div>
+        <input 
+          type="radio" 
+          id="get" 
+          name="method" 
+          value="GET" 
+          checked={method === 'GET'} 
+          onChange={onChange} />
+        <label htmlFor="get">GET</label>
+      </div>
     </div>
+
   );
 };
 
 
 RequestForm.propTypes = {
   url: PropTypes.string.isRequired,
+  method: PropTypes.string.isRequired,
   onSubmit: PropTypes.func.isRequired,
   onChange: PropTypes.func.isRequired
 };
