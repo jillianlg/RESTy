e8c162b59652d1a72c3c4b7421c7cb1b3b3295d6
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-04 16:23:29 -0800

    adds HistoryItem component

---
+++ src/components/RequestForm/History/HistoryList.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HistoryItem = ({}) => {
+  return (
+    <article id={id}>
+      <p id={id}>{method}</p>
+      <span id={id}>{url}</span>
+    </article>
+  )
+}
+
+export default HistoryItem;
