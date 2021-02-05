7eb6fdf09118f4aa40c562eefd5759622a1e836e
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-04 16:54:51 -0800

    adds propTypes to HistoryList componet

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import HistoryItem from '../RequestForm/History/HistoryItem';
 import RequestForm from '../RequestForm/RequestForm';
 import ResponseDisplay from '../ResponseDisplay/ResponseDisplay';
 
 export default function App() {
-  // return <RequestForm />;
-  // return <ResponseDisplay />;
-  return <HistoryItem />;
+  return (
+    <>
+      <RequestForm />
+      <ResponseDisplay />
+    </>
+  );
 }
 
